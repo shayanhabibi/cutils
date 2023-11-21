6c3df7380daab8a2ffe44e40d1a849503515b528
cabboose <shayanftw@gmail.com>
2023-11-21 21:06:38 +0800

    add window manipulation with setwindowpos

--- cutils/winos/windows/windows.nim
+++ cutils/winos/windows/windows.nim
@@ -1,3 +1,37 @@
 import pkg/winim/lean
 
 echo repr FindWindowW(NULL, "winim")
+
+type
+  WindowHandle = object
+    hWnd: HWND
+
+const
+    NotTop = HWND_NOTOPMOST
+    TopMost = HWND_TOPMOST
+    Top = HWND_TOP
+    Bottom = HWND_BOTTOM
+
+proc findWindow*(windowName: string): WindowHandle =
+  let hwnd = FindWindowW(NULL, windowName)
+  return WindowHandle(hWnd: hwnd)
+
+proc setWindowPos(window: WindowHandle, pos: HWND; x,y,cx,cy: int32 = 0): bool {.discardable.} =
+  let uFlags = SWP_NOMOVE or SWP_NOSIZE
+  let res = SetWindowPos(window.hWnd, pos, x,y,cx,cy, UINT(uFlags))
+
+  try:
+    result = bool(res)
+  except:
+    result = false
+
+proc makeTopMost*(win: WindowHandle) =
+  setWindowPos(win, TopMost)
+proc undoTopMost*(win: WindowHandle) =
+  setWindowPos(win, NotTop)
+
+
+when isMainModule:
+  let window = findWindow("winim")
+  # makeTopMost(window)
+  undoTopMost(window)
